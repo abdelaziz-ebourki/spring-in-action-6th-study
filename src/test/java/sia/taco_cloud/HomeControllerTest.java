de991422809cd7050f06d736a1e43b4b8aebf1ab
1abdelaziz <ebourki.azize@gmail.com>
2026-03-12 23:07:26 +0000

    chapter-1: Getting Started
    feat: Initialize Taco Cloud project with Spring Boot, Thymeleaf, and Maven Wrapper
    
    feat: add the home page view, its controller and a simple test

---
+++ src/test/java/sia/taco_cloud/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package sia.taco_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
